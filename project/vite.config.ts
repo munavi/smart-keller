8258a410d4580a32cf69d4e9cf33f495d78378a7
Natalya Murashko <natalya.murashko@hs-augsburg.de>
2022-05-16 13:20:15 +0200

    #1 some clean ups into the code and directory tree

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -5,10 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({reactivityTransform: true})],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias:
+        { '@':      fileURLToPath(new URL('./src',                  import.meta.url)),
+          'config': fileURLToPath(new URL('./src/css/_config.scss', import.meta.url)),
+          '/css':   fileURLToPath(new URL('./src/css',              import.meta.url)),
+        }
   }
 })
